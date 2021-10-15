3313cf29b95557dd024afb4f5413d65b8c06164e
adayal <adayal@vt.edu>
2021-10-15 02:58:56 -0700

    remove dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-const Server_1 = require("./server/Server");
-let app = new Server_1.Server().getApp();
-exports.app = app;
-//# sourceMappingURL=index.js.map
