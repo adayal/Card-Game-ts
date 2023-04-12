0ee933447c5c4b8b4955cc23ed8ddc83595abd66
Amit Dayal <adayal@vt.edu>
2023-04-11 22:39:54 -0700

    fix build

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import constants from '../../common/CONSTANTS'
 import { Button } from './components/Button'
-import { Server, Socket } from "socket.io"
+var io = require("socket.io");
 
 class App extends React.Component<{}, any> {
   constructor() {
